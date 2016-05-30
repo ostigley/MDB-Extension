312ed6e99238d1b42560ca1a50ced5b90902e15c
Oliver Stigley <oliverstigley@gmail.com>
2016-05-30 14:55:39 +1200

    get title file now working in background js script

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
+// function getSelectionText() {var text = "";if (window.getSelection) {text = window.getSelection().toString();} else if (document.selection && document.selection.type != "Control") {text = document.selection.createRange().text;}console.log("Sunday", text);return text}
+
+
+
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(0, {file: 'getTitle1.js'}, function (response) {
+  chrome.tabs.executeScript(null, {file: 'getTitle1.js'}, function (response) {
   	console.log("Monday", response)
   });
 });
--- getTitle1.js
+++ getTitle1.js
@@ -1,11 +1,7 @@
-function getSelectionText() {
-     var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    console.log("Selected", text)
-    return text
+var text
+if (window.getSelection) {
+    text = window.getSelection().toString();
+} else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
 }
-return getSelectionText();
+text
